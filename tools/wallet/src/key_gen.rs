031bbab600393e4fe33de2d3f6ce7ba81d70884d
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-09-14 21:28:32 +0000

    choose where to print mnemonic

--- tools/wallet/src/key_gen.rs
+++ tools/wallet/src/key_gen.rs
@@ -11,45 +11,13 @@ use libra_types::exports::{AccountAddress, AuthenticationKey};
 use std::path::PathBuf;
 
 /// Genereates keys from WalletLibrary, updates a MinerConfig
-pub fn keygen(danger_print: bool) -> (AuthenticationKey, AccountAddress, WalletLibrary, String) {
+pub fn keygen() -> (AuthenticationKey, AccountAddress, WalletLibrary, String) {
     // Generate new keys
     let mut wallet = WalletLibrary::new();
     let mnemonic_string = wallet.mnemonic();
     // NOTE: Authkey uses the child number 0 by default
     let (auth_key, _) = wallet.new_address().expect("Could not generate address");
     let account = auth_key.derived_address();
-    //////////////// Info ////////////////
-
-    if danger_print {
-      println!(
-          "0L Account Address:\n\
-          ...........................\n\
-          {}\n",
-          &account.to_string()
-      );
-
-      println!(
-          "Authentication Key (for key rotation):\n\
-          ...........................\n\
-          {}\n",
-          &auth_key.to_string()
-      );
-
-      println!(
-          "0L mnemonic:\n\
-          ..........................."
-      );
-
-      //use same styles as abscissa_info
-      println!("\x1b[1;36m{}\n\x1b[0m", &mnemonic_string.as_str());
-
-      println!(
-          "WRITE THIS DOWN NOW. This is the last time you will see \
-                    this mnemonic. It is not saved anywhere. Nobody can help \
-                    you if you lose it.\n\n"
-      );
-    }
-
     (auth_key, account, wallet, mnemonic_string)
 }
 
